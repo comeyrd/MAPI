0a37e19b8a9842aecea13273f3d318169355a65c
EYRAUD Come <come.eyraud@telecom-paris.fr>
2023-10-26 23:23:01 +0200

    fix

--- index.mjs
+++ index.mjs
@@ -24,6 +24,15 @@ const app = express();
 app.use(bodyParser.json());
 const port = 3000;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.post("/login", async (req, res) => {
   const { login, password } = req.body;
   try {
